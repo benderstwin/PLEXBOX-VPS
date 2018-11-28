e07502bbaab934c88711c0a4c1d4ee103439ccef
benderstwin <bryan@bwirth.com>
2018-11-28 11:01:40 -0700

    Update step1_adduser.sh

--- step1_adduser.sh
+++ step1_adduser.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-apt update && apt full-upgrade -y
+sudo apt update && apt full-upgrade -y 
+apt install -y apt-transport-https ca-certificates curl software-properties-common 
 read -p "new username: " LINUX_USER
 read -p "new password:" LINUX_PASS
 # quietly add a user without password
-adduser --quiet --disabled-password --shell /bin/bash --home /home/newuser --gecos "User" newuser
+adduser --quiet --disabled-password --shell /bin/bash --home /home/newuser --gecos $LINUX_USER newuser
 # set password
 echo "${LINUX_USER}:${LINUX_PASS}" | chpasswd
 
