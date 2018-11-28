c5fc3149d534ee73b04e6ea58e51ea7d1af6cc49
benderstwin <bryan@bwirth.com>
2018-11-28 11:27:01 -0700

    Update step1_adduser.sh

--- step1_adduser.sh
+++ step1_adduser.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-sudo apt update && apt full-upgrade -y 
-apt install -y apt-transport-https ca-certificates curl software-properties-common 
-read -p "new username: " LINUX_USER
-read -p "new password:" LINUX_PASS
-# quietly add a user without password
-adduser --quiet --disabled-password --shell /bin/bash --home /home/newuser --gecos $LINUX_USER newuser
-# set password
-echo "${LINUX_USER}:${LINUX_PASS}" | chpasswd
+#sudo apt update && apt full-upgrade -y && apt install -y apt-transport-https ca-certificates curl software-properties-common 
+# Script to add a user to Linux system
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+fi
+sudo echo "LINUX_USER=$username" >> /etc/environment
 
-echo "your username is now " ${LINUX_USER} "and password is " ${LINUX_PASS}
+echo "your username is now ${username} and password is ${password}"
